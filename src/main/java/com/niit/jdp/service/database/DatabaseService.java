c434ac6e63f121102d602657169027e725ff9710
Aman Singh <singhaman4570@gmail.com>
2022-09-20 17:19:46 +0530

    added the DatabaseService.java class

--- src/main/java/com/niit/jdp/service/database/DatabaseService.java
+++ src/main/java/com/niit/jdp/service/database/DatabaseService.java
@@ -3,9 +3,7 @@
  *Date:20-09-2022
  *Created With: Intellij IDEA Community Edition
  */
-package com.niit.jdp.model;
+package com.niit.jdp.service.database;
 
-public class Song {
-
-
+public class DatabaseService {
 }
