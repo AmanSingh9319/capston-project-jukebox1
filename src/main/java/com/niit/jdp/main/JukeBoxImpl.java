2585ce9bf300a2610e3d061f18a30f0d631e0426
Aman Singh <singhaman4570@gmail.com>
2022-09-20 21:08:24 +0530

    added the JukeBoxImpl.java class

---
+++ src/main/java/com/niit/jdp/main/JukeBoxImpl.java
@@ -0,0 +1,12 @@
+/*
+ *Author Name: Aman Singh
+ *Date:20-09-2022
+ *Created With: Intellij IDEA Community Edition
+ */
+package com.niit.jdp.main;
+
+public class JukeBoxImpl {
+    public static void main(String[] args) {
+
+    }
+}
