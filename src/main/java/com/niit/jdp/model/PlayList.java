8a807cadc067c5fb644a505b5abc96cb43874082
Aman Singh <singhaman4570@gmail.com>
2022-09-20 19:18:42 +0530

    added the playList class

---
+++ src/main/java/com/niit/jdp/model/PlayList.java
@@ -0,0 +1,9 @@
+/*
+ *Author Name: Aman Singh
+ *Date:20-09-2022
+ *Created With: Intellij IDEA Community Edition
+ */
+package com.niit.jdp.model;
+
+public class PlayList {
+}
--- src/main/java/com/niit/jdp/service/database/DatabaseService.java
+++ src/main/java/com/niit/jdp/service/database/DatabaseService.java
@@ -6,5 +6,6 @@
 package com.niit.jdp.service.database;
 
 public class DatabaseService {
-
+// the url of the database
+private static final String URL = "Jdbc:mysql://localhost:3306/jukebox1";
 }
