90ecf70f680fb9dca5ed71e9741f0eaf7efdfa0a
Aman Singh <singhaman4570@gmail.com>
2022-09-20 17:34:23 +0530

    generate the get and set method

--- src/main/java/com/niit/jdp/model/Song.java
+++ src/main/java/com/niit/jdp/model/Song.java
@@ -24,4 +24,52 @@ public class Song {
         this.artistName = artistName;
         this.genre = genre;
     }
+
+    public int getSongId() {
+        return songId;
+    }
+
+    public void setSongId(int songId) {
+        this.songId = songId;
+    }
+
+    public String getSongName() {
+        return songName;
+    }
+
+    public void setSongName(String songName) {
+        this.songName = songName;
+    }
+
+    public String getSongDuration() {
+        return songDuration;
+    }
+
+    public void setSongDuration(String songDuration) {
+        this.songDuration = songDuration;
+    }
+
+    public String getAlbumName() {
+        return albumName;
+    }
+
+    public void setAlbumName(String albumName) {
+        this.albumName = albumName;
+    }
+
+    public String getArtistName() {
+        return artistName;
+    }
+
+    public void setArtistName(String artistName) {
+        this.artistName = artistName;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
 }
