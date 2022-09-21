ac2c403ad2e417a6ad91aa1ba2be310652a2a883
Aman Singh <singhaman4570@gmail.com>
2022-09-21 10:41:36 +0530

    added the driverManager in database Service class

--- src/main/java/com/niit/jdp/service/DatabaseService.java
+++ src/main/java/com/niit/jdp/service/DatabaseService.java
@@ -5,11 +5,33 @@
  */
 package com.niit.jdp.service;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class DatabaseService {
     // the url of the database
     private static final String URL = "Jdbc:mysql://localhost:3306/jukebox1";
     // the credentials of the user trying to log in to the database
     private static final String USERNAME = "root";
     private static final String PASSWORD = "admin";
+
+    // private field to store the reference of the connection object
+    private Connection databaseConnection;
+
+    public DatabaseService() {
+        this.databaseConnection = null;
+    }
+
+    public Connection getDatabaseConnection() {
+        return databaseConnection;
+    }
+
+    // Create a connection object using the driverManager class
+    public boolean connect() throws ClassNotFoundException, SQLException {
+        databaseConnection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+        return databaseConnection != null;
+    }
+
 
 }
