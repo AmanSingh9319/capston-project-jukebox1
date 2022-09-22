f597eeb51adea81d123e84afa2d251fb5155a336
Aman Singh <singhaman4570@gmail.com>
2022-09-22 21:59:14 +0530

    added the UserDefineException.java class

---
+++ src/main/java/com/niit/jdp/model/UserDefineException.java
@@ -0,0 +1,12 @@
+/*
+ *Author Name: Aman Singh
+ *Date:22-09-2022
+ *Created With: Intellij IDEA Community Edition
+ */
+package com.niit.jdp.model;
+
+public class UserDefineException extends Exception {
+    public UserDefineException(String message) {
+        super(message);
+    }
+}
