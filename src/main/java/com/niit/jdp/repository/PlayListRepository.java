5ca808498c4f85c97aa4c32bf2e28afe68244ce9
Aman Singh <singhaman4570@gmail.com>
2022-09-22 15:32:57 +0530

    add the play list in data base

--- src/main/java/com/niit/jdp/repository/PlayListRepository.java
+++ src/main/java/com/niit/jdp/repository/PlayListRepository.java
@@ -5,6 +5,26 @@
  */
 package com.niit.jdp.repository;
 
+import com.niit.jdp.service.DatabaseService;
+
+import java.sql.Connection;
+
 public class PlayListRepository {
+    /*
+     * given play list add into database
+     * add song into playlist table
+
+     * show all playList from dataBase
+     * get all song from play List
+     */
+
+    DatabaseService connection = new DatabaseService();
+
+    // for adding PlayList into dataBase
+    public void addIntoDatabase(String playListName) {
+        Connection getConnection = connection.connect();
+        String query = "Insert into playList values(?);";
+
+    }
 
 }
